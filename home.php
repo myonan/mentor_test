11704e8281f4438045e9cd45275c8946858c1fe7
anomrac <jgcarmo1@asu.edu>
2021-03-27 22:05:14 -0700

    User specific Mentor info implemented

--- home.php
+++ home.php
@@ -8,6 +8,22 @@ if (!isset($_SESSION['loggedin'])) {
 }
 include 'functions.php';
 
+$DATABASE_HOST = '107.180.1.16:3306';
+$DATABASE_USER = 'group82021';
+$DATABASE_PASS = '2021group8';
+$DATABASE_NAME = '2021group8';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// We don't have the password or email info stored in sessions so instead we can get the results from the database.
+$stmt = $con->prepare('SELECT mentor_name, mentor_email FROM accounts WHERE id = ?');
+// In this case we can use the account ID to get the account info.
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($mentor_name, $mentor_email);
+$stmt->fetch();
+$stmt->close();
 ?>
 
 <!DOCTYPE html>
@@ -57,10 +73,8 @@ include 'functions.php';
         <br>
       
         <h2>MENTOR INFORMATION</h2>
-        <h5>Bob Smith</h5>
-        <p>EMAIL: Bob.Smith@gmail.com <br>
-          PHONE: 555-555-5555
-        </p>
+        <h5><?=$mentor_name?></h5>
+        <p><?=$mentor_email?></p>
       </div>
     </div>
 
